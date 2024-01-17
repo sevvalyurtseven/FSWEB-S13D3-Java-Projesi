52cdc63bb8ae7e901fb127787368d33223e5168e
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-17 17:58:49 +0300

    2. constructor icinde constructor chaining yapildi ve if-else ile custom bir islem tanimlandi.

--- src/Person.java
+++ src/Person.java
@@ -17,7 +17,17 @@ public class Person {
 
     //OVERLOADING:
     public Person(String firstName, String lastName, int age, double salary, boolean isMarried, String[] hobbies){
-        this(firstName, lastName, age);
+        this(firstName, lastName, age); //constructor chaining
+
+       //eklediğim özellikler ile ilgili custom bir işlem yapalim:
+
+        if(salary < 11500){
+            this.salary = 11500;
+        }else{
+            this.salary = salary;
+        }
+        this.isMarried = isMarried;
+        this.hobbies = hobbies;
     }
 }
 
